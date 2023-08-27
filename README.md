7a41aff2c315cd3cd763fb87cd32fb131c09c937
g-wagen <79096832+g-wagen@users.noreply.github.com>
2023-08-27 22:00:29 +0200

    First publish to pypi (#1)
    * feat: Add basic plugin functionality
    
    * docs: fix docstring, version number and extend readme

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-# pdm-plugin
+# PDM Script Complete
 
-> A template for PDM plugin project
+This [PDM](https://github.com/pdm-project/pdm) plugin enables tab completion for pdm scripts.
+
+## Installation
+### PDM
+PDM needs to be installed prior to installing the plugin. Please follow the instructions from the official [PDM README](https://github.com/pdm-project/pdm/README.md).
+
+### The plugin
+You can install the plugin with [PDM](https://github.com/pdm-project/pdm):
+```commandline
+pdm self add pdm-scriptcomplete
+```
+
+As a local-only plugin:
+
+```toml
+# pyproject.toml
+[tool.pdm]
+plugins = ["pdm-scriptcomplete"]
+```
+
+```commandline
+pdm install --plugins
+```
+
+## Usage
+
+```commandline
+pdm script <ENTER>
+```
---
+++ src/pdm_scriptcomplete/__init__.py
@@ -0,0 +1,55 @@
+import argparse
+import readline
+import subprocess
+
+from pdm.cli.commands.base import BaseCommand
+from pdm.core import Core
+from pdm.project import Project
+
+
+class ScriptCompleteCommand(BaseCommand):
+    """
+    Run a PDM script using tab completion
+    """
+
+    arguments = []
+
+    @staticmethod
+    def setup_tab_completion(scripts: list[str]) -> None:
+        def complete(text: str, state: int):
+            matches = []
+            for item in scripts:
+                if item.startswith(text):
+                    matches.append(item)
+
+            if state < len(matches):
+                return matches[state]
+            else:
+                return None
+
+        readline.set_completer(complete)
+        readline.parse_and_bind("tab: complete")
+
+    def handle(self, project: Project, options: argparse.Namespace) -> None:
+        """The command handler function.
+
+        :param project: the pdm project instance
+        :param options: the parsed Namespace object
+        """
+
+        scripts = [str(script) for script in project.scripts]
+        if not scripts:
+            return project.core.ui.echo(message="No scripts found!", err=True)
+
+        self.setup_tab_completion(scripts)
+
+        try:
+            user_input = input(f"Choose script (<TAB> for list): ")
+            subprocess.run(f"pdm {user_input}", shell=True)
+        except KeyboardInterrupt:
+            project.core.ui.echo(message="Choice aborted by user.", err=True)
+
+
+def plugin(core: Core) -> None:
+    """Register pdm plugin to pdm-core."""
+    core.register_command(ScriptCompleteCommand, "script")
